21b4b8adc9e3c83efb25ffc5ac8704de476fbfbd
Ayan Mahapatra <ayanmahapatra4@gmail.com>
2025-08-07 04:09:52 +0530

    update base url

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "https://geniferr-backend.vercel.app/api",
+  baseURL: "https://geniferr-backend.onrender.com",
   withCredentials: true,
 });
 
