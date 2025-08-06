084162903595adfdbdcd9f03a259689bb42dd74f
Ayan Mahapatra <ayanmahapatra4@gmail.com>
2025-08-07 02:49:33 +0530

    still fixing

--- src/utils/upload.js
+++ src/utils/upload.js
@@ -4,12 +4,12 @@ const upload = async (file) => {
   const data = new FormData();
   data.append("file", file);
   data.append("upload_preset", "fiverr");
+  data.append("cloud_name", "da52vtitu"); 
 
   try {
     const res = await axios.post("https://api.cloudinary.com/v1_1/da52vtitu/image/upload", data);
 
-    const { url } = res.data;
-    return url;
+    return res.data.url;
   } catch (err) {
     console.log(err);
   }
